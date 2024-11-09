d41ed6ffa6a1109b03fde9d7a5f143b3c03415ca
xfoff <krzekobrzegoszczynska@gmail.com>
2024-11-09 20:40:52 +0100

    putting the pokemons into an array and splitting it between pages

--- src/dex.tsx
+++ src/dex.tsx
@@ -3,58 +3,84 @@ import { useState, useEffect, ChangeEvent } from 'react';
 import axios from 'axios';
 
 const Dex: React.FC = () => {
-  interface PokemonData {
-    name: string;
-    image: string;
-  }
-
-  const [allPokeData, setAllPokeData] = useState<PokemonData[]>([]);
-  const [searched, setSearched] = useState<string>('');
   const [pokeCount, setPokeCount] = useState<number>(0);
-  const [pokeData, setPokeData] = useState<any>({});
+  const [pokeData, setPokeData] = useState<any>([]);
+  const [allPokeData, setAllPokeData] = useState<any[]>([]);
+  const [page, setPage] = useState<number>(0);
+  const [completed, setCompleted] = useState<boolean>(false);
 
-  const inputChange = (event:ChangeEvent<HTMLInputElement>) => {
-    setSearched(event.target.value);
+  const search = (event:ChangeEvent<HTMLInputElement>) => {
+    const searched = event.target.value;
+    setPokeData(allPokeData.filter(pokemon => pokemon.name.toLowerCase() == searched));
   };
 
   const fetchPokemonCount = async() => {
     await axios.get("https://pokeapi.co/api/v2/pokemon?limit=0")
-      .then(response => setPokeCount(response.data.count))
-  }
-
-  const fetchPokemonData = async() => {
-    await axios.get(`https://pokeapi.co/api/v2/pokemon/${searched}`)
-      .then(response => setPokeData(response.data))
-  }
-
-  const fetchAllPokemonData = async() => {
-    setAllPokeData([]);
-    await axios.get("https://pokeapi.co/api/v2/pokemon?limit=35")
+      .then(response => {
+        const count = response.data.count
+        setPokeCount(count)
+        return axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${count}`)
+      })
       .then(response => {
         const pokemons:any = response.data.results.map((pokemon:any) => axios.get(pokemon.url))
 
         Promise.all(pokemons)
-          .then(pokemonData => pokemonData.map((pokemon:any) => {
-            setAllPokeData(prevData => [...prevData, {name: pokemon.data.species.name, image: pokemon.data.sprites.front_default}])
-          }))
+          .then(pokemonData => {
+            const data = pokemonData.map(pokemon => pokemon.data);
+            setPokeData(data.slice(0, 35));
+            setAllPokeData(data);
+            setCompleted(true);
+          });
       })
+  }
+
+  const changePage = (by:number) => {
+    setPage(prevPage => {
+      const newPage = ((prevPage + by) % (Math.round(pokeCount / 35) + 1) + (Math.round(pokeCount / 35) + 1)) % (Math.round(pokeCount / 35) + 1);
+      setPokeData(allPokeData.slice(newPage * 35, newPage * 35 + 35));
+      return newPage;
+    })
+  }
+
+  const rotateCard = (event:any, index:number) => {
+    const card = document.getElementById(`${index}`);
+    if (card) {
+      const rect = card.getBoundingClientRect();
+      const rotateY = Math.round((event.clientX - (rect.left + rect.width / 2)) / 2);
+      const rotateX = Math.round(-(event.clientY - (rect.top + rect.height / 2)) / 2);
+
+      card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    }
+  }
+
+  const resetCardRotation = (index:number) => {
+    const card = document.getElementById(`${index}`);
+    if (card) card.style.transform = 'rotateX(0deg) rotateY(0deg)';
   }
 
   useEffect(() => {
     fetchPokemonCount();
-    fetchAllPokemonData();
   }, []);
 
   return (
     <div className="main">
-      <input type="text" className="search" onChange={inputChange} onKeyDown={(event) => {if (event.key == 'Enter') fetchPokemonData()}}/>
+      <div className="completed" style={completed ? {display: 'none'} : {display: 'block'}}>The pokemons are on their way...</div>
+      <div className="search">
+        <input type="text" onChange={search} placeholder="Search a pokemon..." style={completed ? {display: 'block'} : {display: 'none'}}/>
+        <button style={completed ? {display: 'block'} : {display: 'none'}}>?</button>
+      </div>
         <div className="cards">
-          {allPokeData.map((pokemon:any, index:number) => (
-            <div key={index} className="card">
-              <img src={pokemon.image} alt={pokemon.name}/>
+          {pokeData.map((pokemon:any, index:number) => (
+            <div id={`${index}`} key={index} className="card" onMouseMove={(event) => rotateCard(event, index)} onMouseLeave={() => resetCardRotation(index)}>
+              <img src={pokemon.sprites.front_default} alt="could not fetch image"/>
               <p>{pokemon.name}</p>
             </div>
           ))}
+        </div>
+        <div className="arrows" style={completed ? {display: 'flex'} : {display: 'none'}}>
+          <span onClick={() => changePage(-1)}>{"<"}</span>
+          <p>page {page} of {Math.round(pokeCount / 35)}</p>
+          <span onClick={() => {changePage(1)}}>{">"}</span>
         </div>
     </div>
   )
