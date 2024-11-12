f8c25ae67260ca442318c20de60691837974ccf7
xfoff <krzekobrzegoszczynska@gmail.com>
2024-11-12 18:17:41 +0100

    even more fixes

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://xfoff.github.io/poke-e-dex',
+  base: '/poke-e-dex/',
 })
